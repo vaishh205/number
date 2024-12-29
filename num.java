8c72616b39240dae0d76f6f02396de13d78a21e0
vaishh205 <vaishhhshevate@gmail.com>
2024-12-29 14:15:42 +0530

    Update num.java

--- num.java
+++ num.java
@@ -1,6 +1,6 @@
 public class num {
     public static void main(String[] args) {
-        for(init i =1; i<=10;i++)
+        for(int i =1; i<=10;i++)
         {
         System.out.println(i);
         }
